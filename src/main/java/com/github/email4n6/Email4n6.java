da0ecb22732546c5957207282799e54c9c3a7d4a
Marten4n6 <Marten4n6@protonmail.com>
2018-11-18 12:57:21 +0100

    Change shutdown button text

--- src/main/java/com/github/email4n6/Email4n6.java
+++ src/main/java/com/github/email4n6/Email4n6.java
@@ -17,24 +17,22 @@
  */
 package com.github.email4n6;
 
-import java.io.File;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.Optional;
-
 import com.github.email4n6.model.Version;
 import com.github.email4n6.utils.PathUtils;
 import com.github.email4n6.view.StartupStage;
 import com.github.email4n6.view.TabbedScene;
-
 import javafx.application.Application;
 import javafx.application.Platform;
 import javafx.scene.control.Alert;
-import javafx.scene.control.ButtonBar;
 import javafx.scene.control.ButtonType;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 import lombok.extern.slf4j.Slf4j;
+
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Optional;
 
 /**
  * This is the starting class.
@@ -44,33 +42,36 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class Email4n6 extends Application {
 
-	public static void main(String[] args) {
-		log.info("Starting Email4n6 v{}...", Version.VERSION_NUMBER);
+    public static void main(String[] args) {
+        log.info("Starting Email4n6 v{}...", Version.VERSION_NUMBER);
 
-		launch(args);
-	}
+        launch(args);
+    }
 
-	@Override
-	public void start(Stage stage) {
-		confirmLiveInCurrentDirectory();
-		createDirectories();
+    @Override
+    public void start(Stage stage) {
+        confirmLiveInCurrentDirectory();
+        createDirectories();
 
-		// Show the main scene.
-		stage.setTitle("Email4n6 v" + Version.VERSION_NUMBER);
+        // Show the main scene.
+        stage.setTitle("Email4n6 v" + Version.VERSION_NUMBER);
         stage.setScene(new TabbedScene().getScene());
         stage.centerOnScreen();
         stage.show();
         stage.setOnCloseRequest((event) -> {
-            Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+            Alert alert = new Alert(
+                    Alert.AlertType.CONFIRMATION,
+                    "Are you sure you want to quit?",
+                    ButtonType.YES, ButtonType.NO
+            );
             alert.setTitle("Email4n6 v" + Version.VERSION_NUMBER);
             alert.setHeaderText(null);
-            alert.setContentText("Are you sure you want to quit?");
             alert.initOwner(stage);
             alert.initModality(Modality.WINDOW_MODAL);
 
             Optional<ButtonType> result = alert.showAndWait();
 
-            if (result.isPresent() && result.get() == ButtonType.OK) {
+            if (result.isPresent() && result.get() == ButtonType.YES) {
                 log.info("Shutting down...");
 
                 Platform.exit();
@@ -79,23 +80,23 @@ public class Email4n6 extends Application {
                 log.info("Shutdown cancelled.");
                 event.consume();
             }
-		});
-	}
+        });
+    }
 
-	private void confirmLiveInCurrentDirectory() {
-		if (!Files.exists(Paths.get(PathUtils.getCasesPath()))) {
-			StartupStage startupStage = new StartupStage();
-			startupStage.showAndWait();
+    private void confirmLiveInCurrentDirectory() {
+        if (!Files.exists(Paths.get(PathUtils.getCasesPath()))) {
+            StartupStage startupStage = new StartupStage();
+            startupStage.showAndWait();
 
-			if (!startupStage.isConfirmed()) {
-				Platform.exit();
-				System.exit(0);
-			}
-		}
-	}
+            if (!startupStage.isConfirmed()) {
+                Platform.exit();
+                System.exit(0);
+            }
+        }
+    }
 
-	private void createDirectories() {
-		new File(PathUtils.getCasesPath()).mkdir();
-		new File(PathUtils.getTempPath()).mkdir();
-	}
+    private void createDirectories() {
+        new File(PathUtils.getCasesPath()).mkdir();
+        new File(PathUtils.getTempPath()).mkdir();
+    }
 }
